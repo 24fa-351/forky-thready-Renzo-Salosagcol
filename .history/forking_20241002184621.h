a64648d4584902f19319f5c9928c66d9dfedcfb6
renzoSalosagcol <renzosalosagcol@csu.fullerton.edu>
2024-10-02 19:25:23 -0700

    Basic running program with Makefile. Fork is still undefined

---
+++ .history/forking_20241002184621.h
@@ -0,0 +1,8 @@
+#ifndef FORKING_H
+#define FORKING_H
+
+void forking1(int num_things);
+void forking2();
+void forking3();
+
+#endif
---
+++ .history/main_20241002184254.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int random_sleep_time() {
+  return 1 + rand() % (8);
+}
+
+int main(int argc, char *argv[]) {
+  const int num_things = atoi(argv[1]);
+  const int pattern_num = atoi(argv[2]);
+
+  pid_t pid = fork();
+  printf("Number of things: %d, Pattern Number: %d\n", num_things, pattern_num);
+  return 0;
+}
