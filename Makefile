e80dd447abe3a48f718f9980a23199e2839473aa
Renzo-Salosagcol <renzosalosagcol@outlook.com>
2024-12-11 15:29:24 -0800

    Added argc checking + fixed pattern 2

--- Makefile
+++ Makefile
@@ -10,17 +10,10 @@ test: main.o
 	./main 5 1
 
 print: main.o
-	echo "Running ./main with args 10 and 1\n" >> results.txt
-	./main 10 1 > results.txt
-
-	echo "\nRunning ./main with args 10 and 2:\n" >> results.txt
-	./main 10 2 >> results.txt
-
-	echo "\nRunning ./main with args 5 and 1\n" >> results.txt
-	./main 5 1 >> results.txt
-
-	echo "\nRunning ./main with args 5 and 2\n" >> results.txt
-	./main 5 2 >> results.txt
+	./main 10 1
+	./main 10 2
+	./main 5 1
+	./main 5 2
 
 time: main.o
 	time ./main
--- main.c
+++ main.c
@@ -12,9 +12,9 @@ void pattern1(int num_things) {
   for (int process_num = 1; process_num <= num_things; process_num++) {
       if (fork() == 0) {
           // Child process
-          printf("Process %d (PID: %d) beginning\n", process_num, getpid());
+          fprintf(stderr, "Process %d (PID: %d) beginning\n", process_num, getpid());
           sleep(random_sleep_time());
-          printf("Process %d (PID: %d) exiting\n", process_num, getpid());
+          fprintf(stderr, "Process %d (PID: %d) exiting\n", process_num, getpid());
           exit(0);
       }
   }
@@ -23,38 +23,56 @@ void pattern1(int num_things) {
   while (wait(NULL) > 0);
 }
 
-void pattern2(int num_things) {
-  for (int process_num = 1; process_num <= num_things; process_num++) {
-      pid_t pid = fork();
-      if (pid == 0) {
-          // Child process
-          printf("Process %d (PID: %d) beginning\n", process_num, getpid());
-          sleep(random_sleep_time());
-          printf("Process %d (PID: %d) exiting\n", process_num, getpid());
-          exit(0);
-      } else {
-          // Parent process prints creating message before child process begins
-          if (process_num < num_things) {
-              printf("Process %d creating Process %d\n", process_num, process_num + 1);
-          }
+void pattern2(int num_things, int process_num) {
 
-          // Parent process waits for the child to complete
-          wait(NULL);
+  while (process_num <= num_things) {
+    if (fork() == 0) {
+      // Child process
+      fprintf(stderr, "Process %d (PID: %d) beginning\n", process_num, getpid());
+
+      if (process_num < num_things) {
+        fprintf(stderr, "Process %d creating Process %d\n", process_num, process_num + 1);
+        pattern2(num_things, process_num + 1);
       }
+      
+      sleep(random_sleep_time());
+      fprintf(stderr, "Process %d (PID: %d) exiting\n", process_num, getpid());
+      exit(0);
+    }
+    else {
+      // Parent process waits for the child to complete
+      wait(NULL);
+      break;
+    }
   }
+  // Parent process waiting for children to exit
+  while (wait(NULL) > 0);
 }
 
 int main(int argc, char *argv[]) {
+  if (argc != 3) {
+      fprintf(stderr, "Usage: %s <num_things> <pattern_num>\n", argv[0]);
+      return 1;
+  }
+
   const int num_things = atoi(argv[1]);
   const int pattern_num = atoi(argv[2]);
+  FILE *results = freopen("results.txt", "a", stderr);
+  setvbuf(stderr, NULL, _IONBF, 0);
+
+  fprintf(stderr, "Running ./main with args %u and %d\n\n", num_things, pattern_num);
 
   srand(time(NULL));
 
   if (pattern_num == 1) {
     pattern1(num_things);
   } else if (pattern_num == 2) {
-    pattern2(num_things);
+    pattern2(num_things, 1);
   }
 
+  fprintf(stderr, "\nPROGRAM COMPLETE\n");
+  fprintf(stderr, "\n******************************\n");
+
+  fclose(results);
   return 0;
 }
